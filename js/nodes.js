8681959381db2b54547890e6aca1878f704e99d8
Alejandro Vasquez <josejaguer12@gmail.com>
2023-01-25 20:59:17 -0600

    se cambio el css a la metodologia bem

--- js/nodes.js
+++ js/nodes.js
@@ -1,5 +1,5 @@
 const btnMenu = document.querySelector(".header__top i");
-const menuList = document.querySelector("ul");
+const menuList = document.querySelector(".header__list");
 
 const menuToggle = () => {
   menuList.classList.toggle("none");
